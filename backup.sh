d541cd4e6cec5b2b2e9d1b6890a8a615f2477708
hemstar7 <fadlyweb@gmail.com>
2018-02-08 17:37:11 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -5,9 +5,9 @@ Konfigurasi backup MySQL dari SSH (Bash)
 4. Isi file:
 #!/bin/bash
 echo "Menghubungkan ke server";
-ssh username_ssh@domain.com -t 'mysqldump -u username_mysql -pPasswdMysql --all-databases > /home/user/homes/mysqlbackup.sql;exit';
+ssh username_ssh@domain.com -t 'mysqldump -u username_mysql -pPasswdMysql --all-databases > /home/username_ssh/homes/mysqlbackup.sql;exit';
 echo "Ambil file backup";
-scp usernam_ssh@domain.com:/home/username/homes/mysqlbackup.sql /home/userlocalanda/mysql_$(date +%Y%m%d_%H%M%S).sql;
+scp username_ssh@domain.com:/home/username/homes/mysqlbackup.sql /home/userlocalanda/mysql_$(date +%Y%m%d_%H%M%S).sql;
 echo "Selesai Backup ..";
 5. Simpan dan buat persimission execute dengan cara chmod +x /opt/backupmysql.sh     
 6. Buat cron untuk execute  /opt/backupmysql.sh                                                                                               
